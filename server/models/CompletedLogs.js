2af981cacb1351adb283249d6c7a94a14c326722
Harvey John <harveyabello@gmail.com>
2024-08-09 10:26:09 +0800

    Modified CompletedLog Model

--- server/models/CompletedLogs.js
+++ server/models/CompletedLogs.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const CompletedLogSchema = new mongoose.Schema({
   docId: { type: mongoose.Schema.Types.ObjectId, ref: 'docs', required: true },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'users', required: true },
-  receivingLogs: [{ type: mongoose.Schema.Types.ObjectId, ref: 'ReceivingLog' }],
-  forwardingLogs: [{ type: mongoose.Schema.Types.ObjectId, ref: 'ForwardingLog' }],
+ // receivingLogs: [{ type: mongoose.Schema.Types.ObjectId, ref: 'ReceivingLog' }],
+//forwardingLogs: [{ type: mongoose.Schema.Types.ObjectId, ref: 'ForwardingLog' }],
   completedAt: { type: Date, default: Date.now }
 });
 
